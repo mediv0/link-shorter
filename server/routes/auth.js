f3afab756d71a5127bff4cd9171903c0d656965e
mediv0 <anonymousaxis@gmail.com>
2021-06-07 23:55:32 +0430

    feat: create login and sign up routes

---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const authController = require("../controllers/auth");
+
+router.post("/login", authController.postLogin);
+router.post("/signup", authController.postSignup);
+
+module.exports = router;
