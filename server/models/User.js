9030bbe73679e76287858205915389751c115c72
mediv0 <anonymousaxis@gmail.com>
2021-06-07 21:13:37 +0430

    feat: create Url model

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongodb");
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("users", UserSchema);
+
+exports.User = User;
