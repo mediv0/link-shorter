0cb9efe86e78c18c49d702fdc0ca7523d397b10b
mediv0 <anonymousaxis@gmail.com>
2021-06-08 21:36:41 +0430

    test: add tests for login controller

---
+++ server/controllers/__tests__/login.spec.js
@@ -0,0 +1,60 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const User = require("../../models/User");
+let server;
+
+const dummyUser = {
+    email: "anonymous@gmail.com",
+    password: "$2b$12$Jzx/Bz3j5QxE9IYoQDgVweNkC0urA1IKYMSsnRmBpEDVnNVx.WJGa",
+};
+
+async function createDummyUser() {
+    await User.collection.insertOne(dummyUser);
+}
+
+describe("/api/auth/login", () => {
+    beforeEach(async () => {
+        server = require("../../index");
+        server = await server.handler.listen(3001);
+
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.DB_URL, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: true,
+            });
+        }
+
+        await User.collection.deleteMany({});
+        await createDummyUser();
+    });
+
+    afterEach(async () => {
+        await server.close();
+        await mongoose.connection.close();
+    });
+
+    it("should login", async () => {
+        const res = await request(server)
+            .post("/auth/login")
+            .send({
+                email: "anonymous@gmail.com",
+                password: "Xxx445709",
+            })
+            .set("Accept", "application/json");
+
+        expect(res.status).toBe(200);
+    });
+
+    it("should fail (invalid password or email)", async () => {
+        const res = await request(server)
+            .post("/auth/login")
+            .send({
+                email: "anonymous@gmail.com",
+                password: "Xxx",
+            })
+            .set("Accept", "application/json");
+
+        expect(res.body).toEqual({ message: "Your email or password is incorrect.", status: "error", statusCode: 401 });
+    });
+});
