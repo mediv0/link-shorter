7d2f330c36eb9dcb320a253971648ace82e33f3c
mediv0 <anonymousaxis@gmail.com>
2021-06-09 03:48:53 +0430

    feat: add login page

--- components/auth/login.vue
+++ components/auth/login.vue
@@ -3,12 +3,13 @@
         <modal-header><span class="font-700">Login</span> to your Account</modal-header>
         <form class="mt-50">
             <label for="email" class="textbox_label">email</label>
-            <input type="text" name="email" class="textbox" />
+            <input v-model="email" type="text" name="email" class="textbox" />
             <label for="password" class="textbox_label label2">password</label>
-            <input type="password" name="password" class="textbox" />
-            <btn class="mt-15">Login</btn>
+            <input v-model="password" type="password" name="password" class="textbox" />
+            <btn class="mt-15" :is-loading="isLoading" @click="login">Login</btn>
         </form>
         <!-- spinner here -->
+        <p v-if="hasError" class="errorMsg">{{ errorMsg }}</p>
     </div>
 </template>
 
@@ -19,6 +20,37 @@ export default {
     components: {
         modalHeader: header,
         btn: button,
+    },
+
+    data() {
+        return {
+            email: "",
+            password: "",
+
+            hasError: false,
+            errorMsg: "something went wrong...",
+            isLoading: false,
+        };
+    },
+
+    methods: {
+        async login() {
+            this.isLoading = true;
+            try {
+                await this.$axios.post("/api/auth/login", {
+                    email: this.email,
+                    password: this.password,
+                });
+
+                this.$store.commit("setIsAuth", true);
+            } catch (e) {
+                const { response } = e;
+                this.errorMsg = response.data.message;
+                this.hasError = true;
+            }
+
+            this.isLoading = false;
+        },
     },
 };
 </script>
