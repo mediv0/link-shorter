236ce0363ce38ed66321308799f54fbb32b9d471
mediv0 <anonymousaxis@gmail.com>
2021-06-09 03:49:54 +0430

    feat: add _url route for redirect

---
+++ pages/l/_url.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>hello world</div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    async asyncData({ params }) {
+        const slug = params.url;
+        return { slug };
+    },
+    created() {
+        // const url = await this.$axios.get("/api/")
+
+        console.log(this.slug);
+        console.log(this.this.$route);
+    },
+};
+</script>
